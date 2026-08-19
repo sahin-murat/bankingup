5d024b0fdb62b9bc8536c3e7908627a2d46f23e1
sahin-murat <murat.sanih@gmail.com>
2026-08-19 07:42:54 +0200

    database layer added

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,14 +5,19 @@ import (
 	"strings"
 )
 
-const defaultServerAddress = ":8080"
+const (
+	defaultServerAddress = ":8080"
+	defaultDatabaseURL   = "postgresql://postgres:postgres@localhost:5432/postgres"
+)
 
 type Config interface {
 	GetServerAddress() string
+	GetDatabaseURL() string
 }
 
 type config struct {
 	serverAddress string
+	databaseURL   string
 }
 
 func New() (*config, error) {
@@ -21,11 +26,21 @@ func New() (*config, error) {
 		serverAddress = defaultServerAddress
 	}
 
+	databaseURL := strings.TrimSpace(os.Getenv("DATABASE_URL"))
+	if databaseURL == "" {
+		databaseURL = defaultDatabaseURL
+	}
+
 	return &config{
 		serverAddress: serverAddress,
+		databaseURL:   databaseURL,
 	}, nil
 }
 
 func (c *config) GetServerAddress() string {
 	return c.serverAddress
+}
+
+func (c *config) GetDatabaseURL() string {
+	return c.databaseURL
 }
---
+++ internal/database/database.go
@@ -0,0 +1,54 @@
+package database
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Config interface {
+	GetDatabaseURL() string
+}
+
+type database struct {
+	gormDB *gorm.DB
+	sqlDB  *sql.DB
+}
+
+func New(cfg Config) (*database, error) {
+	databaseURL := cfg.GetDatabaseURL()
+
+	gormDB, err := gorm.Open(postgres.Open(databaseURL))
+	if err != nil {
+		return nil, fmt.Errorf("can not create gorm db with database url: %s, %w", databaseURL, err)
+	}
+
+	sqlDB, err := gormDB.DB()
+	if err != nil {
+		return nil, fmt.Errorf("can not get Postgresql connection: %w", err)
+	}
+
+	return &database{
+		gormDB: gormDB,
+		sqlDB:  sqlDB,
+	}, nil
+}
+
+func (d *database) Ping(ctx context.Context) error {
+	if err := d.sqlDB.PingContext(ctx); err != nil {
+		return fmt.Errorf("can not ping PostgreSQL database: %w", err)
+	}
+
+	return nil
+}
+
+func (d *database) Close() error {
+	if err := d.sqlDB.Close(); err != nil {
+		return fmt.Errorf("can not close PostgreSQL database: %w", err)
+	}
+
+	return nil
+}
